1565d7fc2f86f16a41d71888b63dde2554daf9f9
Tankettekek <64431301+Tankettekek@users.noreply.github.com>
2024-12-06 21:39:01 +0100

    fix

---
+++ src/main/java/nl/cfgroningen/bot/KattisVisualSummaryGenerator.java
@@ -0,0 +1,23 @@
+package nl.cfgroningen.bot;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class KattisVisualSummaryGenerator {
+//    private KattisBot bot;
+
+    public BufferedImage generateVisualSummary() {
+        BufferedImage image = new BufferedImage(1080, 1920, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.createGraphics();
+
+        // g.setColor(Color.)
+        //
+
+        return image;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
