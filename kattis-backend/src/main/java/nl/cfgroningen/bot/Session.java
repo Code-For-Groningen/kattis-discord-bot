4714e7c7cff07656a0d7a11cc938c844c5f6000f
justtryingthingsout <31097061+justtryingthingsout@users.noreply.github.com>
2025-03-10 13:55:28 +0100

    fix: Temporarily fix graph by removing broken code

--- kattis-backend/src/main/java/nl/cfgroningen/bot/Session.java
+++ kattis-backend/src/main/java/nl/cfgroningen/bot/Session.java
@@ -20,103 +20,88 @@ import java.util.function.Function;
 @Log
 @Data
 public class Session {
-    @JsonIgnore
-    @Setter(AccessLevel.NONE)
-    @Getter(AccessLevel.NONE)
-    private Timer timer = new Timer();
+  @JsonIgnore
+  @Setter(AccessLevel.NONE)
+  @Getter(AccessLevel.NONE)
+  private Timer timer = new Timer();
 
-    private List<UniversityScoreInformation> infos = new ArrayList<>();
-    private Map<String, UniversityUserInformation> init;
+  private List<UniversityScoreInformation> infos = new ArrayList<>();
 
-    private Set<Map.Entry<Long, Long>> summaryMessageIds = new HashSet<>();
+  private Set<Map.Entry<Long, Long>> summaryMessageIds = new HashSet<>();
 
-    @AllArgsConstructor
-    private class InformationFetcher extends TimerTask {
-        private KattisDataManager dataManager;
-        private KattisBot bot;
+  @AllArgsConstructor
+  private class InformationFetcher extends TimerTask {
+    private KattisDataManager dataManager;
+    private KattisBot bot;
 
-        @Override
-        public void run() {
-            // Update the session with the latest information
-            dataManager.getFreshUniversityStats(bot.getOwningUniversityUrl()).whenComplete((i, e) -> {
-                if (e == null) {
-                    if (infos.size() > 10) {
-                        infos.remove(0);
-                    }
-                    for (UniversityUserInformation student : i.getStudents()) {
-                        student.setScore(
-                            student.getScore() - init.get(student.getName()).getScore()
-                        );
-                    }
-                    infos.add(i);
-                } else {
-                    log.warning("Failed to fetch university stats: " + e.getMessage());
-                }
-            });
+    @Override
+    public void run() {
+      // Update the session with the latest information
+      dataManager.getFreshUniversityStats(bot.getOwningUniversityUrl()).whenComplete((i, e) -> {
+        if (e == null) {
+          infos.add(i);
+        } else {
+          log.warning("Failed to fetch university stats: " + e.getMessage());
         }
+      });
     }
+  }
 
-    @AllArgsConstructor
-    private class SummaryUpdater extends TimerTask {
-        private KattisBot bot;
-        private KattisVisualSummaryGenerator visualSummaryGenerator;
-        private Function<BufferedImage, EmbedBuilder> embedBuilderFunction;
+  @AllArgsConstructor
+  private class SummaryUpdater extends TimerTask {
+    private KattisBot bot;
+    private KattisVisualSummaryGenerator visualSummaryGenerator;
+    private Function<BufferedImage, EmbedBuilder> embedBuilderFunction;
 
-        @Override
-        public void run() {
-            // Generate the visual summary
-            BufferedImage image = visualSummaryGenerator.generateVisualSummary(infos);
+    @Override
+    public void run() {
+      // Generate the visual summary
+      BufferedImage image = visualSummaryGenerator.generateVisualSummary(infos);
 
-            for (Map.Entry<Long, Long> messageUid : summaryMessageIds) {
-                long messageId = messageUid.getKey();
-                long channelId = messageUid.getValue();
+      for (Map.Entry<Long, Long> messageUid : summaryMessageIds) {
+        long messageId = messageUid.getKey();
+        long channelId = messageUid.getValue();
 
+        BufferedImage stupidDiscordFixer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
 
-                BufferedImage stupidDiscordFixer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-
-                bot.getClient().getTextChannelById(channelId)
-                        .ifPresent(channel -> channel.getMessageById(messageId).thenAcceptAsync(
-                                message -> message.createUpdater()
-                                        .removeAllEmbeds()
-                                        .removeExistingAttachments()
-                                        .addAttachment(stupidDiscordFixer, "_.png")
-                                        .addEmbed(embedBuilderFunction.apply(image))
-                                        .applyChanges()));
-            }
-        }
+        bot.getClient().getTextChannelById(channelId)
+            .ifPresent(channel -> channel.getMessageById(messageId).thenAcceptAsync(
+                message -> message.createUpdater()
+                    .removeAllEmbeds()
+                    .removeExistingAttachments()
+                    .addAttachment(stupidDiscordFixer, "_.png")
+                    .addEmbed(embedBuilderFunction.apply(image))
+                    .applyChanges()));
+      }
     }
+  }
 
-    public void startSession(
-            KattisDataManager dataManager,
-            KattisBot bot,
-            KattisVisualSummaryGenerator summaryGenerator,
-            Function<BufferedImage, EmbedBuilder> embedBuilderFunction
-    ) throws ExecutionException, InterruptedException, TimeoutException {
-        CompletableFuture<UniversityScoreInformation> future = dataManager
-                .getFreshUniversityStats(bot.getOwningUniversityUrl());
+  public void startSession(
+      KattisDataManager dataManager,
+      KattisBot bot,
+      KattisVisualSummaryGenerator summaryGenerator,
+      Function<BufferedImage, EmbedBuilder> embedBuilderFunction)
+      throws ExecutionException, InterruptedException, TimeoutException {
+    CompletableFuture<UniversityScoreInformation> future = dataManager
+        .getFreshUniversityStats(bot.getOwningUniversityUrl());
 
-        // To even start the session we need at least the first info
-        UniversityScoreInformation info = future.get(10, TimeUnit.SECONDS);
-        init = new HashMap<String, UniversityUserInformation>();
-        for (UniversityUserInformation student : info.getStudents()) {
-            init.put(student.getName(), student);
-            student.setScore(0); //set delta
-        }
+    // To even start the session we need at least the first info
+    UniversityScoreInformation info = future.get(10, TimeUnit.SECONDS);
+    infos.add(info);
+    timer.schedule(new InformationFetcher(dataManager, bot), 0, 5000);
+    timer.schedule(new SummaryUpdater(bot, summaryGenerator, embedBuilderFunction), 0, 10000);
+  }
 
-        timer.schedule(new InformationFetcher(dataManager, bot), 0, 5000);
-        timer.schedule(new SummaryUpdater(bot, summaryGenerator, embedBuilderFunction), 0, 10000);
+  public void stopSession(KattisBot bot) {
+    timer.cancel();
+
+    // Go through the messages and delete them
+    for (Map.Entry<Long, Long> messageUid : summaryMessageIds) {
+      long messageId = messageUid.getKey();
+      long channelId = messageUid.getValue();
+
+      bot.getClient().getTextChannelById(channelId)
+          .ifPresent(channel -> channel.getMessageById(messageId).thenAcceptAsync(Deletable::delete));
     }
-
-    public void stopSession(KattisBot bot) {
-        timer.cancel();
-
-        // Go through the messages and delete them
-        for (Map.Entry<Long, Long> messageUid : summaryMessageIds) {
-            long messageId = messageUid.getKey();
-            long channelId = messageUid.getValue();
-
-            bot.getClient().getTextChannelById(channelId)
-                    .ifPresent(channel -> channel.getMessageById(messageId).thenAcceptAsync(Deletable::delete));
-        }
-    }
+  }
 }
